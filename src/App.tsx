16b88e8537be606c817d61a1ac07edf195f67800
Rafal Nalewajek <rafal.nalewajek@gmail.com>
2023-03-19 18:55:16 +0100

    Added protected route to home page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,35 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
-import { Login } from './componets/Login/Login';
-import { Register } from './componets/Register/Register';
-import { Home } from './componets/Home/Home';
+import { Loader } from './componets/Commpare/Loader/Loader';
+import { ProtectedRoute } from './componets/ProtectedRoute/ProtectedRoute';
+import { UserProvider } from './Contexts/UserContext/UserProvider';
 
 import './App.css';
 
+const Home = React.lazy(() => import('./componets/Home/Home'));
+const Login = React.lazy(() => import('./componets/Login/Login'));
+const Register = React.lazy(() => import('./componets/Register/Register'));
 
 export const App = () => {
   return (
-    <div className='App'>
+    <UserProvider>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/test" element={<Navigate to={"/login"} />} />
-        </Routes>
+        <Suspense fallback={<Loader />}>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <ProtectedRoute>
+                  <Home />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/*" element={<Navigate to="/login" replace />} />
+          </Routes>
+        </Suspense>
       </BrowserRouter>
-    </div>
+    </UserProvider>
   );
 };
