82fd34c268e21e7aa00b5a8d9b5d72ff6f2bb96a
Rafal Nalewajek <rafal.nalewajek@gmail.com>
2023-03-20 21:20:49 +0100

    Added Home Component.

--- src/componets/Home/Home.tsx
+++ src/componets/Home/Home.tsx
@@ -1,16 +1,24 @@
 import React from 'react';
+import { Aside } from './Aside/Aside';
+import { Container } from './Container/Container';
+import { Header } from './Header/Header';
+import { Main } from './Main/Mian';
+import { ProjectResponseData } from 'types';
 
-// import './Home.css';
+import './Home.css';
+
+import data from '../../data/tablesData.json';
 
 const Home = () => {
-  return <>Home</>;
+  const tableData = data as ProjectResponseData;
+
+  return (
+    <Container>
+      <Header id={tableData.id} title={tableData.title} />
+      <Aside />
+      <Main cards={tableData.cards} />
+    </Container>
+  );
 };
 
 export default Home;
-
-// <Container>
-//   <Header />
-//   <Aside />
-//   <Main />
-// </Container>
-// );
