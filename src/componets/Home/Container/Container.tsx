7a8b59e7d2b644da50c9a535dca33e0a9f844103
Rafal Nalewajek <rafal.nalewajek@gmail.com>
2023-03-21 07:53:05 +0100

    Added Container component.

---
+++ src/componets/Home/Container/Container.tsx
@@ -0,0 +1,21 @@
+import React, { CSSProperties, ReactNode } from 'react';
+
+import './Container.css';
+
+type ContainerProps = {
+  children: ReactNode;
+};
+
+export const Container = ({ children }: ContainerProps) => {
+  const urlBgImage = `./images/backgrounds/bg-1.jpg`;
+
+  const styles: CSSProperties = {
+    backgroundImage: `url(${urlBgImage})`,
+  };
+
+  return (
+    <div className="container" style={styles}>
+      {children}
+    </div>
+  );
+};
