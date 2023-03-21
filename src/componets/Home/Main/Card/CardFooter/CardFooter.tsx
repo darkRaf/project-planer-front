be3d019fbdbf29190c454bd24b52eafb451ebb16
Rafal Nalewajek <rafal.nalewajek@gmail.com>
2023-03-21 07:59:41 +0100

    Added CardFooter component.

---
+++ src/componets/Home/Main/Card/CardFooter/CardFooter.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './CardFooter.css';
+
+export function CardFooter() {
+  return (
+    <div className="card-footer">
+      <button type='button' className="card-btn-add">&#43; Dodaj zadanie</button>
+    </div>
+  );
+}
