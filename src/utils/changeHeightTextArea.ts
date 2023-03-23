1c3377707a8266113a38776123ac9513e0320b2c
Rafal Nalewajek <rafal.nalewajek@gmail.com>
2023-03-23 13:31:21 +0100

    Corrected function for checking the automatic height of the textarea.

--- src/utils/changeHeightTextArea.ts
+++ src/utils/changeHeightTextArea.ts
@@ -1,12 +1,9 @@
-import { Dispatch, SetStateAction, ChangeEvent } from 'react';
+import { ChangeEvent } from 'react';
 
 export const changeHeightTextArea = (
   e: ChangeEvent<HTMLTextAreaElement>,
-  clb: Dispatch<SetStateAction<string>>
 ) => {
   const area = e.target;
-
-  clb(area.value);
 
   area.style.height = 'auto';
   area.style.height = `${area.scrollHeight}px`;
