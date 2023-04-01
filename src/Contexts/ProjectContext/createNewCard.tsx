641a58b5d832b7d3f706f24e1798f42e677b8cac
Rafal Nalewajek <rafal.nalewajek@gmail.com>
2023-04-01 17:59:57 +0200

    Deleted files.

--- src/Contexts/ProjectContext/createNewCard.tsx
+++
@@ -1,9 +0,0 @@
-import { v4 as uuid } from 'uuid';
-import { CardResponse } from 'types';
-
-export const createNewCard = (title: string): CardResponse => ({
-  id: uuid(),
-  title,
-  tasksId: [],
-  tasks: [],
-});
