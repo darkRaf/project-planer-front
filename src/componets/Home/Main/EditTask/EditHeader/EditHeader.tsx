befb476ec14e026b1f65dd692f731938d5a2f20a
Rafal Nalewajek <rafal.nalewajek@gmail.com>
2023-04-02 20:29:35 +0200

    Added task title edit.

--- src/componets/Home/Main/EditTask/EditHeader/EditHeader.tsx
+++ src/componets/Home/Main/EditTask/EditHeader/EditHeader.tsx
@@ -1,19 +1,82 @@
-import React, { useContext} from 'react';
+import React, { ChangeEvent, useContext, useEffect, useRef, useState } from 'react';
+import { UserContext } from '../../../../../Contexts/UserContext/UserContext';
 import { ProjectContext } from '../../../../../Contexts/ProjectContext/ProjectContext';
+import { MAX_TASK_LENGTH } from '../../../../../settings/settings';
+import { changeHeightTextArea } from '../../../../../utils/changeHeightTextArea';
+import { checkClickOutSide } from '../../../../../utils/checkClickOutSide';
+import { TaskEntity } from 'types';
 import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
 
 import './EditHeader.css';
 
 type EditHeaderProps = {
   title: string;
+  changeData: (key: keyof TaskEntity, val: string) => void;
 };
 
-export const EditHeader = ({ title }: EditHeaderProps) => {
+export const EditHeader = ({ title, changeData }: EditHeaderProps) => {
+  const { setErrorHandle } = useContext(UserContext);
   const { setShowModalEditTask } = useContext(ProjectContext);
 
+  const [titleTask, setTitleTask] = useState('');
+  const [showForm, setShowForm] = useState(false);
+  const [isSelect, setIsSelect] = useState(true);
+
+  const areaRef = useRef<HTMLTextAreaElement>(null);
+
+  useEffect(() => {
+    if (showForm) {
+      areaRef.current?.focus();
+      isSelect && areaRef.current?.select();
+      document.addEventListener('mousedown', mousedownHandle);
+      setIsSelect(false);
+    }
+
+    return () => {
+      document.removeEventListener('mousedown', mousedownHandle);
+    };
+  });
+
+  useEffect(() => {
+    setTitleTask(title);
+  }, [title]);
+
+  const addTitleTask = () => {
+    changeData('title', titleTask);
+    setShowForm(false);
+    setIsSelect(true);
+  };
+
+  const onChangeHandle = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    const { value } = e.target;
+
+    if (value.length > MAX_TASK_LENGTH) {
+      setErrorHandle(`Przekroczono ${MAX_TASK_LENGTH} znaków!`);
+      return;
+    }
+
+    setTitleTask(value);
+    changeHeightTextArea(e);
+  };
+
+  const mousedownHandle = (e: MouseEvent) => {
+    if (checkClickOutSide(e, areaRef)) return;
+
+    if (titleTask.length) {
+      addTitleTask();
+      return;
+    }
+
+    setShowForm(false);
+  };
+
   return (
-    <div className="edit-task-header">
-      <h2 className="edit-title">{title}</h2>
+    <div className="edit-task-header" onClick={() => setShowForm(true)}>
+      {showForm ? (
+        <textarea ref={areaRef} className="card-input" value={titleTask} onChange={onChangeHandle} />
+      ) : (
+        <h2 className="edit-title">{titleTask}</h2>
+      )}
       <div className="edit-task-clouse-box" onClick={() => setShowModalEditTask('')}>
         <CloseRoundedIcon />
       </div>
