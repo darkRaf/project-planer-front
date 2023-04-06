a77dd429084e787dac7ddf6ebdfbc38282768d6a
Rafal Nalewajek <rafal.nalewajek@gmail.com>
2023-04-06 14:48:28 +0200

    Added display of settings or edits from the header level.

--- src/componets/Home/Header/Header.tsx
+++ src/componets/Home/Header/Header.tsx
@@ -1,6 +1,8 @@
 import React, { useContext } from 'react';
 import { UserContext } from '../../../Contexts/UserContext/UserContext';
 import { ModalTypes, ProjectContext } from '../../../Contexts/ProjectContext/ProjectContext';
+import AddRoundedIcon from '@mui/icons-material/AddRounded';
+import SettingsRoundedIcon from '@mui/icons-material/SettingsRounded';
 
 import './Header.css';
 
@@ -17,7 +19,17 @@ export const Header = (props: HeaderProps) => {
 
   const showUserMenuHandle = () => {
     showModal === ModalTypes.UserMenu ? setShowModal(ModalTypes.None) : setShowModal(ModalTypes.UserMenu);
-  }
+  };
+
+  const showNewProjectHandle = () => {
+    setShowModal(ModalTypes.NewProject);
+  };
+
+  const showSettinsProjectHandle = () => {
+    console.log('project settings'); // TODO: ustawienia
+
+    // setShowModal(ModalTypes.NewProject);
+  };
 
   return (
     <header className="header">
@@ -25,7 +37,22 @@ export const Header = (props: HeaderProps) => {
         <div className="header-logo">pp</div>
         <span className="header-app-name">Project Planer</span>
       </div>
-      <div className="header-section table-name">{props.title || 'Dodaj nowy projekt'}</div>
+      <div
+        className="header-section table-name"
+        onClick={props.title ? showSettinsProjectHandle : showNewProjectHandle}
+      >
+        {props.title ? (
+          <>
+            <span className="project-name">{props.title}</span>
+            <SettingsRoundedIcon />
+          </>
+        ) : (
+          <>
+            <AddRoundedIcon />
+            <span className="project-name">Dodaj nowy projekt</span>
+          </>
+        )}
+      </div>
       <div className="header-section">
         <div className="header-avatar-box" onClick={showUserMenuHandle}>
           <img src={`./images/avatars/${avatarImg}`} alt="Avatar" />
