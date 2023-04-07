89209ebae02e19dfc625938441d4b54341c609eb
Rafal Nalewajek <rafal.nalewajek@gmail.com>
2023-04-07 18:55:13 +0200

    Settings to App.

--- src/settings/settings.example.ts
+++ src/settings/settings.example.ts
@@ -5,6 +5,10 @@ export const MAX_PROJECT_TITLE_LENGTH = 50;
 export const MAX_CARD_TITLE_LENGTH = 50;
 export const MAX_TASK_DESCRIPTION_LENGTH = 1000;
 
+export const MAX_USER_NAME = 20;
+export const MAX_USER_LASTNAME = 30;
+export const MAX_USER_PASSWORD = 20;
+
 export const API_LINK = 'https://api.openweathermap.org/data/2.5/weather?';
 // https://openweathermap.org/api
 export const API_KEY = '*********************';
